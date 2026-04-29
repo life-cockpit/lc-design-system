a8e3f0c8892d145a3f3573a328d40e413ae4e487
fritzcoded <eric.fritzsche@life-cockpit.de>
2026-04-29 13:31:07 +0200

    fix: move docs infra to tools/infra and update deploy workflow
    - Create standalone CDK stack for docs (S3 + CloudFront)
    - Update deploy-docs.yml to reference tools/infra
    - Remove dependency on deleted tools/mcp-server/infra

---
+++ tools/infra/lib/docs-stack.ts
@@ -0,0 +1,48 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Construct } from 'constructs';
+import * as path from 'path';
+
+export class DocsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = new s3.Bucket(this, 'DocsBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    const distribution = new cloudfront.Distribution(this, 'DocsDistribution', {
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(bucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployDocs', {
+      sources: [
+        s3deploy.Source.asset(path.join(__dirname, '..', '..', '..', 'dist', 'apps', 'demo', 'browser')),
+      ],
+      destinationBucket: bucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+
+    new cdk.CfnOutput(this, 'DocsUrl', {
+      value: `https://${distribution.distributionDomainName}`,
+      description: 'Docs CloudFront URL',
+    });
+  }
+}
